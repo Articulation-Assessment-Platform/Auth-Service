bb1b93068d825cb60f3f7ccd42ac88dc3952555d
Meek,Maike M.Z.E. <m.meek@student.fontys.nl>
2024-06-17 12:53:21 +0200

    add

---
+++ AuthService/AuthRepositoryLayer/Entities/UserCredentialsEntity.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Text.Json.Serialization;
+
+namespace AuthRepositoryLayer.Entities
+{
+    public class UserCredentialsEntity
+    {
+        public int Id {  get; set; }
+        public int UserID { get; set; }     
+        public string Email { get; set; }
+        public string HashedPassword { get; set; }
+        public Byte[] Salt { get; set; }
+        public string Role {  get; set; }
+    }
+}
---
+++ AuthService/AuthServiceLayer/Subscriptions/ServiceBusHostedService.cs
@@ -0,0 +1,27 @@
+﻿namespace AuthServiceLayer.Subscriptions
+{
+    public class ServiceBusHostedService : IHostedService
+    {
+        private readonly ServiceBusMessageProcessor _messageProcessor;
+        private readonly ILogger<ServiceBusHostedService> _logger;
+
+        public ServiceBusHostedService(ServiceBusMessageProcessor messageProcessor, ILogger<ServiceBusHostedService> logger)
+        {
+            _messageProcessor = messageProcessor;
+            _logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Starting Azure Service Bus message processing...");
+            return _messageProcessor.StartProcessingAsync(cancellationToken);
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Stopping Azure Service Bus message processing...");
+            return _messageProcessor.StopProcessingAsync();
+        }
+    }
+
+}
