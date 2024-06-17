6d0d6f8b45d5294e8ed1688c5d0c17f0130d9176
Meek,Maike M.Z.E <m.meek@student.fontys.nl>
2024-06-17 09:38:29 +0200

    gitignore

---
+++ AuthService/AuthRepositoryLayer/Repository/AuthRepository.cs
@@ -0,0 +1,31 @@
+﻿using AuthRepositoryLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthRepositoryLayer.Repository
+{
+    public class AuthRepository : IAuthRepository
+    {
+        protected readonly AuthContext _context;
+
+        public AuthRepository(AuthContext context)
+        {
+            _context = context;
+        }
+
+        public UserCredentialsEntity GetUserByEmail(string email)
+        {
+            return _context.Users.FirstOrDefault(u => u.Email == email);
+        }
+        public async Task<UserCredentialsEntity> AddAsync(UserCredentialsEntity entity)
+        {
+            await _context.Set<UserCredentialsEntity>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public void DeleteAsync(UserCredentialsEntity entity)
+        {
+            _context.Remove(entity);
+        }
+    }
+}
---
+++ AuthTests/AuthService/AuthServiceTests.cs
@@ -0,0 +1,51 @@
+﻿using AuthServiceLayer.Models;
+using AuthServiceLayer.Services;
+using AuthRepositoryLayer.Entities;
+using AuthRepositoryLayer.Repository;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AuthServiceLayer.Tests
+{
+    public class AuthServiceTests
+    {
+        private IAuthService _authService;
+        private Mock<IAuthRepository> _authRepositoryMock;
+        private Mock<IOptions<JwtSettings>> _jwtSettingsMock;
+        private Mock<IConfiguration> _configurationMock;
+        private Mock<PasswordHasherService> _passwordHasherMock;
+
+        public AuthServiceTests()
+        {
+            _authRepositoryMock = new Mock<IAuthRepository>();
+            _jwtSettingsMock = new Mock<IOptions<JwtSettings>>();
+            _configurationMock = new Mock<IConfiguration>();
+            _passwordHasherMock = new Mock<PasswordHasherService>();
+
+            _authService = new AuthService(_jwtSettingsMock.Object, _configurationMock.Object, _authRepositoryMock.Object, _passwordHasherMock.Object);
+        }
+
+
+
+   
+
+
+        [Fact]
+        public void DeleteUser_ValidEmail_SuccessfullyDeleted()
+        {
+            // Arrange
+            var email = "test@example.com";
+            var userEntity = new UserCredentialsEntity { Email = email };
+            _authRepositoryMock.Setup(x => x.GetUserByEmail(email)).Returns(userEntity);
+
+            // Act
+            _authService.DeleteUser(email);
+
+            // Assert
+            _authRepositoryMock.Verify(x => x.DeleteAsync(userEntity), Times.Once);
+        }
+    }
+}
